5e18f9f923e12d8ac513845367c7a03d62dc0f5b
Mikalay Radchuk <radchuk.m@gmail.com>
2015-11-10 22:53:19 +0300

    SASS: migrate to SCSS (first iteration)

--- src/base/settings/defaults.py
+++ src/base/settings/defaults.py
@@ -121,6 +121,10 @@ LOCALE_PATHS = (
 STATIC_URL = '/static/'
 STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'gulp'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
 
