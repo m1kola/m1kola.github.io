f1fc5a147f3ea9cdfff2a6de35dd3c8fed27a965
Mikalai Radchuk <radchuk.m@gmail.com>
2016-09-01 00:14:06 +0300

    Returns BlogIndexPage.
    I don't want to have a lot of pages underneath the homepage.

--- src/blog/content/blog/models.py
+++ src/blog/content/blog/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import redirect
 from modelcluster.contrib import taggit
 from modelcluster.fields import ParentalKey
 from taggit.models import TaggedItemBase
@@ -10,6 +11,26 @@ from blog.base.blocks import StoryBlock
 from blog.base.models import BaseFields
 
 
+class BlogIndexPage(Page):
+    """
+    I just want to have "/blog" prefix for blog
+    and don't want to have a lot of child pages underneath the homepage.
+    """
+
+    # Only allow creating BlogIndexPage at the root level
+    parent_page_types = ['home.HomePage']
+
+    # Only allow to create a one page instance
+    @classmethod
+    def can_create_at(cls, parent):
+        return super().can_create_at(parent) and not cls.objects.count()
+
+    def serve(self, request, *args, **kwargs):
+        parent = self.get_parent()
+
+        return redirect(parent.url, permanent=True)
+
+
 class BlogPageTag(TaggedItemBase):
     content_object = ParentalKey('BlogPage', related_name='tagged_items')
 
@@ -19,6 +40,9 @@ class BlogPage(Page, BaseFields):
     body = wt_fields.StreamField(StoryBlock())
     tags = taggit.ClusterTaggableManager(through=BlogPageTag, blank=True)
 
+    parent_page_types = ['blog.BlogIndexPage']
+    subpage_types = []
+
     content_panels = Page.content_panels + [
         edit_handlers.FieldPanel('lead', classname='full'),
         edit_handlers.StreamFieldPanel('body'),
@@ -27,6 +51,3 @@ class BlogPage(Page, BaseFields):
     promote_panels = Page.promote_panels + BaseFields.promote_panels + [
         edit_handlers.FieldPanel('tags'),
     ]
-
-    parent_page_types = ['home.HomePage']
-    subpage_types = []
