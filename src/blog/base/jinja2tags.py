a76deff316bb3ed678024ea652f4042857140554
Mikalai Radchuk <radchuk.m@gmail.com>
2016-09-01 22:47:44 +0300

    Replace the main menu with a context function
    The context function renders main menu based based on the pages
    which sit underneath the home page and which have show_in_menus flag

--- src/blog/base/jinja2tags.py
+++ src/blog/base/jinja2tags.py
@@ -1,31 +1,47 @@
+import jinja2
+import jinja2.ext
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.template import defaultfilters
+from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils import translation
 from django.utils.datetime_safe import datetime
-from jinja2.ext import Extension
+from django.utils.safestring import mark_safe
+from wagtail.wagtailcore.models import Page
 
 
-class ProjectCoreExtension(Extension):
+class ProjectCoreExtension(jinja2.ext.Extension):
     def __init__(self, environment):
         super(ProjectCoreExtension, self).__init__(environment)
 
         self.environment.globals.update({
             'static': staticfiles_storage.url,
             'url': reverse,
+            'now': datetime.now,
+            'primary_nav': primary_nav,
             'settings': {
                 'SITE_TITLE': settings.SITE_TITLE,
                 'SOCIAL_MEDIA_URLS': settings.SOCIAL_MEDIA_URLS,
                 'FOOTER_COPYRIGHT': settings.FOOTER_COPYRIGHT,
             },
-            'now': datetime.now,
         })
         self.environment.filters.update({
             'date': defaultfilters.date,
             'timesince': defaultfilters.timesince_filter,
         })
         self.environment.install_gettext_translations(translation)
+
+
+@jinja2.contextfunction
+def primary_nav(context):
+    homepage = Page.objects.get(slug='home')
+    menu_items = homepage.get_children().live().filter(show_in_menus=True)
+
+    return mark_safe(render_to_string('base/tags/primary_nav.html', {
+        'menu_items': menu_items,
+        'request': context['request'],
+    }))
 
 
 # Nicer import names
