d926939fc60278fa3b2feaefc2fb8ec026ecdf30
Mikalai Radchuk <radchuk.m@gmail.com>
2016-09-03 00:15:13 +0300

    Adds google tag manager code

--- src/blog/base/jinja2tags.py
+++ src/blog/base/jinja2tags.py
@@ -1,4 +1,3 @@
-import jinja2
 import jinja2.ext
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
@@ -20,6 +19,7 @@ class ProjectCoreExtension(jinja2.ext.Extension):
             'url': reverse,
             'now': datetime.now,
             'primary_nav': primary_nav,
+            'google_tag_manager': google_tag_manager,
             'settings': {
                 'SITE_TITLE': settings.SITE_TITLE,
                 'SOCIAL_MEDIA_URLS': settings.SOCIAL_MEDIA_URLS,
@@ -41,6 +41,15 @@ def primary_nav(context):
     return mark_safe(render_to_string('base/tags/primary_nav.html', {
         'menu_items': menu_items,
         'request': context['request'],
+    }))
+
+
+@jinja2.contextfunction
+def google_tag_manager(context):
+    google_tag_manager_id = getattr(settings, 'GOOGLE_TAG_MANAGER_ID', None)
+
+    return mark_safe(render_to_string('base/tags/google_tag_manager.html', {
+        'google_tag_manager_id': google_tag_manager_id,
     }))
 
 
--- src/blog/base/settings/prod.py
+++ src/blog/base/settings/prod.py
@@ -4,6 +4,8 @@ from .base import *  # NOQA
 # Just to make sure
 DEBUG = False
 
+GOOGLE_TAG_MANAGER_ID = 'GTM-PVR4H5'
+
 
 try:
     from .local import *  # noqa
