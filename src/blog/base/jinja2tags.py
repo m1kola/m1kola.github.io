121dea5e87d240abe31d7cab1556b7ca1941951f
Mikalai Radchuk <radchuk.m@gmail.com>
2016-09-04 17:37:26 +0300

    Add og:* tags and refactor heading templates

--- src/blog/base/jinja2tags.py
+++ src/blog/base/jinja2tags.py
@@ -20,6 +20,9 @@ class ProjectCoreExtension(jinja2.ext.Extension):
             'now': datetime.now,
             'primary_nav': primary_nav,
             'google_tag_manager': google_tag_manager,
+            'get_title_display': get_title_display,
+            'get_description_display': get_description_display,
+            'get_image_display': get_image_display,
             'settings': {
                 'SITE_TITLE': settings.SITE_TITLE,
                 'SOCIAL_MEDIA_URLS': settings.SOCIAL_MEDIA_URLS,
@@ -53,5 +56,45 @@ def google_tag_manager(context):
     }))
 
 
-# Nicer import names
+@jinja2.contextfunction
+def get_title_display(context, page=None):
+    title = settings.SITE_TITLE
+
+    page = page or context.get('page')
+    if page:
+        title = page.title
+        if page.seo_title:
+            title = page.seo_title
+
+    return title
+
+
+@jinja2.contextfunction
+def get_description_display(context, page=None):
+    description = ''
+
+    page = page or context.get('page')
+    if page:
+        description = page.title
+        if page.search_description:
+            description = page.search_description
+
+    return description
+
+
+@jinja2.contextfunction
+def get_image_display(context, page=None):
+    image = staticfiles_storage.url('base/images/default_bg.jpg')
+
+    page = page or context.get('page')
+    if page and page.main_image:
+        image = page.main_image.get_rendition('width-1024').url
+
+    request = context.get('request')
+    if request:
+        image = request.build_absolute_uri(image)
+
+    return image
+
+# Nicer import name
 core = ProjectCoreExtension
