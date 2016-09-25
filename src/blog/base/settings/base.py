872dc2e45398328da8950690cca2bcbf8c68584b
Mikalai Radchuk <radchuk.m@gmail.com>
2016-09-25 21:57:02 +0300

    Add logging config

--- src/blog/base/settings/base.py
+++ src/blog/base/settings/base.py
@@ -175,6 +175,55 @@ if 'WAGTAILSEARCH_BACKENDS_DEFAULT_URLS' in cfg_env:
     }
 
 
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+        'console': {
+            'level': 'ERROR',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'formatters': {
+        'default': {
+            'verbose': '[%(asctime)s] (%(process)d/%(thread)d) %(name)s %(levelname)s: %(message)s'
+        }
+    },
+    'loggers': {
+        'blog': {
+            'handlers': ['mail_admins', 'console'],
+            'level': 'INFO',
+            'propagate': False,
+            'formatter': 'verbose',
+        },
+        'wagtail': {
+            'handlers': ['mail_admins', 'console'],
+            'level': 'INFO',
+            'propagate': False,
+            'formatter': 'verbose',
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'console'],
+            'level': 'ERROR',
+            'propagate': False,
+            'formatter': 'verbose',
+        },
+        'django.security': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+            'formatter': 'verbose',
+        },
+    },
+}
+
+
 # Site constants
 
 SITE_TITLE = 'mikola.by'
