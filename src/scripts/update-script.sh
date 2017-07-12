9bf104bb991133e3509943290c8fa6d8b346690c
Mikalai Radchuk <radchuk.m@gmail.com>
2017-07-12 14:58:44 +0100

    Don't run update_index because we don't use search

--- src/scripts/update-script.sh
+++ src/scripts/update-script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 python manage.py migrate --noinput && \
-python manage.py collectstatic --noinput && \
-python manage.py update_index
+python manage.py collectstatic --noinput
