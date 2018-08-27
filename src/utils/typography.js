45fbd6fdfc573d39f4b037bdc4e2a18dca3a6848
Mikalai Radchuk <radchuk.m@gmail.com>
2018-08-27 13:00:40 +0300

    Gatsby init commit

---
+++ src/utils/typography.js
@@ -0,0 +1,18 @@
+import Typography from 'typography'
+import theme from 'typography-theme-lincoln'
+
+theme.overrideThemeStyles = () => ({
+  'a.gatsby-resp-image-link': {
+    backgroundImage: 'none',
+  },
+})
+
+
+const typography = new Typography(theme)
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== 'production') {
+  typography.injectStyles()
+}
+
+export default typography
