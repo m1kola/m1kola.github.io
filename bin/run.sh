449ba6eaf43de40fc4e95575d1cca73093fdb609
Mikalai Radchuk <radchuk.m@gmail.com>
2018-08-27 12:53:14 +0300

    Removes old stuff

--- bin/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-set -e
-
-# Run migrations
-python manage.py migrate --noinput
-
-# Run CMD
-exec "$@"
--- src/blog/content/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from .feeds import BlogFeed
-
-urlpatterns = [
-    url(r'^feed/$', BlogFeed(), name='feed'),
-]
